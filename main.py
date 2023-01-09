aeaf6853a17e8c47cee05f54ce8cc6e9eeb7483f
Alexander Shad <50224102+AlexSkeleton12@users.noreply.github.com>
2023-01-09 23:31:10 +0100

    IT FUCKING WORKS WOOHOO

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ reddit = praw.Reddit(
     client_id=redditKeys["PUS"],
     client_secret=redditKeys["Secret"],
     password=redditKeys["Password"],
+    user_agent="",
     username=redditKeys["Username"]
 )
 
-# print(reddit.user.me())
+print(reddit.user.me())
 
-acc1 = "AlexanderShad4" # Example Twitter Account
+acc1 = "" # Example Twitter Account
 
 latestTweet1 = API.user_timeline(screen_name=acc1, count=1)[0].text
 
@@ -26,6 +27,7 @@ def eventSource1(latestTweet1):
     if latestTweet1 != API.user_timeline(screen_name=acc1, count=1)[0].text:
 
         # Trigger Reddit Post
+        reddit.subreddit("").submit(selftext="", title=API.user_timeline(screen_name=acc1, count=1)[0].text)
         print("New Tweet: " + API.user_timeline(screen_name=acc1, count=1)[0].text)
 
         # Reset Loop
